5605619e11210ae737e556fb78edd74cc5400ed7
vvarmaaddanki <varmakbm9@gmail.com>
2026-09-05 02:34:34 +0530

    Fix Django allowed hosts

--- backend/coreBackend/settings.py
+++ backend/coreBackend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-local-development-key-123456789"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = [
     "127.0.0.1",
